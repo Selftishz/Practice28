29e0f6c6d3e6876dfbf556ff1f43b1b2a05fd761
Selftishz <art2sys048@gmail.com>
2022-12-25 21:23:42 +0300

    dada

--- src/ExampleSortSet.java
+++ src/ExampleSortSet.java
@@ -1,5 +1,24 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+public class ExampleSortSet
+{
+    public static void main(String[] args)
+    {
+        HashSet<String> set = new HashSet<>();
+        set.add("java");
+        set.add("is");
+        set.add("the best");
+        set.add("language");
+        Set<String> treeSet = getInstance(set);
+        System.out.println(treeSet);
+
+    }
+    public static <T> Set<T> getInstance(Set<T> hashSet)
+    {
+        Set<T> treeSet = hashSet.stream().collect(Collectors.toCollection(TreeSet::new));
+        return treeSet;
     }
 }
